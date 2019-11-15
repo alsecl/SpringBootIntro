395a30c23e78c04047a11378ad410eba1f3dd101
alsecl <alexander.clauss@itgain.de>
2019-11-15 12:18:31 +0100

    Simple Get-Mapping with parameter.

--- src/main/java/de/itgain/springboot/intro/controller/IntroRestController.java
+++ src/main/java/de/itgain/springboot/intro/controller/IntroRestController.java
@@ -3,6 +3,7 @@ package de.itgain.springboot.intro.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/intro")
 public class IntroRestController {
 
-	@GetMapping("/hello")
-	public ResponseEntity<String> hello() {
-		return new ResponseEntity<>("Hello visitor.", HttpStatus.OK);
+	@GetMapping("/hello/{name}")
+	public ResponseEntity<String> hello(@PathVariable String name) {
+		return new ResponseEntity<>("Hello " + name + ".", HttpStatus.OK);
 	}
 
 }
