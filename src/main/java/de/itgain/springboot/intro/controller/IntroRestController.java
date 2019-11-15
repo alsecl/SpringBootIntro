165bdf5ff9f95930e3d94cfcb86a003586a028da
alsecl <alexander.clauss@itgain.de>
2019-11-15 13:23:47 +0100

    Added Logging.

--- src/main/java/de/itgain/springboot/intro/controller/IntroRestController.java
+++ src/main/java/de/itgain/springboot/intro/controller/IntroRestController.java
@@ -1,5 +1,7 @@
 package de.itgain.springboot.intro.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
@@ -14,11 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @PropertySource("classpath:intro.properties")
 public class IntroRestController {
 
+	private static final Logger log = LoggerFactory.getLogger(IntroRestController.class);
+
 	@Value("${greeting.text}")
 	private String greetingText;
 
 	@GetMapping("/hello/{name}")
 	public ResponseEntity<String> hello(@PathVariable String name) {
+		log.info("Received hello from {}", name);
+
 		return new ResponseEntity<>(greetingText + name, HttpStatus.OK);
 	}
 
